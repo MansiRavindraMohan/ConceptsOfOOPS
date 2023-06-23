0a922d3fffb4cc84a9de90db90f7ff8cbbd71d52
MansiRavindraMohan <manasimohan29@gmail.com>
2023-06-23 21:23:14 +0530

    [Mansi]: Example of Object and Class

---
+++ ConceptsOfOOPS/ClassAndObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsOfOOPS
+{
+    public class ClassAndObject
+    {
+        public void MethodClass()
+        {
+            Console.WriteLine("This is example of class and object");
+        }
+    }
+}
---
+++ ConceptsOfOOPS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsOfOOPS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassAndObject classAndObject = new ClassAndObject();
+            classAndObject.MethodClass();
+            Console.ReadLine();
+        }
+    }
+}
