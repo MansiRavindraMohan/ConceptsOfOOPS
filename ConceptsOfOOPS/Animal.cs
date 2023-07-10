81ff732dc5fdf289fd800a07523d2c1204b754a0
MansiRavindraMohan <manasimohan29@gmail.com>
2023-07-10 11:11:00 +0530

    [Mansi]: Program for example of polymorphism (Method overriding and overloading)

---
+++ ConceptsOfOOPS/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsOfOOPS
+{
+    public class Animal
+    {
+        public virtual void Sound()
+        {
+            Console.WriteLine("This is parent class called Animal");
+        }
+    }
+    public class Dog : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Dogs bark");
+        }
+    }
+}
--- ConceptsOfOOPS/Program.cs
+++ ConceptsOfOOPS/Program.cs
@@ -16,9 +16,26 @@ namespace ConceptsOfOOPS
             //VariableTypes variableTypes = new VariableTypes();
             //variableTypes.VariableMethod();
 
-            MethodTypes methodTypes= new MethodTypes();
-            methodTypes.NonParameterMethod();
-            methodTypes.ParameterMethod(25,"Komal");
+            //MethodTypes methodTypes= new MethodTypes();
+            //methodTypes.NonParameterMethod();
+            //methodTypes.ParameterMethod(25,"Komal");
+
+            //Car car = new Car();
+            //car.Start();
+            //car.Stop();
+            //Console.WriteLine(car.carName = "XUV");
+            //Console.WriteLine(car.year = 2022);
+            //car.Drive();
+
+            //1)Method Overloading
+            Rectangle rectangle = new Rectangle();
+            rectangle.PrintArea(3, 5);
+            rectangle.PrintArea(5);
+            rectangle.PrintArea(6, 2.5);
+            rectangle.PrintArea(5.5);
+            //2)Method Overriding
+            Dog dog = new Dog();
+            dog.Sound();
             Console.ReadLine();
         }
     }
